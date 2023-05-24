49f74c9fedade7c1226299f642c58a3e2b493575
Lucas <lucas.7795.la@gmail.com>
2023-05-24 19:43:45 -0300

    Configuração de dependencias e mapeamento de entidade JPA

---
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -0,0 +1,26 @@
+package med.voll.api.medico;
+
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import med.voll.api.endereco.Endereco;
+
+@Entity(name = "Medico")
+@Table(name = "medicos")
+@Getter
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Medico {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String crm;
+    @Enumerated(EnumType.STRING)
+    private Especialidade especialidade;
+    @Embedded
+    private Endereco endereco;
+}
